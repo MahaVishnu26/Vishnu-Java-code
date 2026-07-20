01f7ce2722de0c2ddb8c58f3658068aa28ade737
MahaVishnu26 <mahamahavishnu83241@gmail.com>
2026-07-20 15:26:46 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,10 @@
+public class maha{
+    public static void main (String[]args){
+        int[]marks = {480,470,465,478,397};
+        String[] name = {"vishnu","karthi","kiran","dharun","machi"};
+        for(int i = 0; i<marks.length;i++)
+        {
+            System.out.println(name[i]+"="+marks[i]);
+        }
+    }
+}
