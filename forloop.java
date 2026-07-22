6a5a44c7c7a0072a3288f54c089d58423dbfd509
MahaVishnu26 <mahamahavishnu83241@gmail.com>
2026-07-22 18:24:27 +0530

    Create forloop.java

---
+++ forloop.java
@@ -0,0 +1,11 @@
+public class length{
+    public static void main(String[] args){
+     for (int i=2006;i<=2040;i++){
+
+        if(i%6==0){
+            System.out.println("vishnu");
+            System.out.println("77");
+        }
+     }
+    }
+}
